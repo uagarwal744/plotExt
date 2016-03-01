f3667f33cf63002730d31258a080ccb85f67d90f
Hemachandra Kolisetty <kolisetty.hemachandra510@gmail.com>
2016-03-01 16:37:12 +0000

    to convert pdf to image

--- pdf_to_img.py
+++ pdf_to_img.py
@@ -2,10 +2,10 @@ import PythonMagick
 from pyPdf import PdfFileReader
 
 img = PythonMagick.Image()
-def pdf_to_img(input_file, density):
+def pdf_to_img(input_file, density): #give density in string format("200")
 	img.density(density)
-	my_file = PdfFileReader(file("{0}","r").format(input_file))
-	numPages= myfile.getNumPages()
+	my_file = PdfFileReader(file("{0}".format(input_file),"r"))
+	numPages= my_file.getNumPages()
 	for i in range(numPages):
 		img.read("{0}[{1}]".format(input_file,i))
 		img.write("{0}_image{1}.jpg".format(input_file,i))
