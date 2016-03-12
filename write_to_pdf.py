f9bb2676fdf23d16546a3cf803d96d27f3674de8
Nayan Raju <v.nayanraju@gmail.com>
2016-03-12 22:45:26 +0530

    pdf output

--- write_to_pdf.py
+++ write_to_pdf.py
@@ -4,7 +4,6 @@ from reportlab.lib.units import inch,cm
 from reportlab.lib.utils import ImageReader
 import numpy as np
 
-#Name of Output PDF
 x='new.pdf'
 
 #Size of Canvas
@@ -12,90 +11,79 @@ c=canvas.Canvas(x,pagesize=A4)
 width, height=A4
 
 
-#New Page...
-c.translate(0,height)
+def pdfoutput(img,ar):
+	#Name of Output PDF
+	global z
+	global x
+	global c
+	global width
+	global height
+	
+	#New Page...
+	c.translate(0,height)
 
-c.setFont("Times-Roman", 30)
-c.drawCentredString(width/2,-.5*inch,"Input Page")
+	c.setFont("Times-Roman", 30)
+	c.drawCentredString(width/2,-.5*inch,"Input Page "+ str(z+1))
 
-#Image Print
-im=ImageReader('b.jpg')
-w=im.getSize()[0]
-h=im.getSize()[1]
-r=(width-2*cm)/w
-w= width-2*cm
-c.drawImage(im,(width-w)/2,-(height+h*r)/2,w,r*h)
+	#Image Print
+	im=ImageReader(img)
+	w=im.getSize()[0]
+	h=im.getSize()[1]
+	r=(width-2*cm)/w
+	w= width-2*cm
+	c.drawImage(im,(width-w)/2,-(height+h*r)/2,w,r*h)
 
 
-#Save Page
-c.showPage()
+	#Save Page
+	c.showPage()
 
-#New Page
-c.translate(0,height)
-c.setFont("Times-Roman", 30)
-c.drawCentredString(width/2,-.3*inch,"Output Page")
-c.setFont("Times-Roman",14)
-#Array to be printed
+	#New Page
+	c.translate(0,height)
+	c.setFont("Times-Roman", 30)
+	c.drawCentredString(width/2,-.3*inch,"Output Page "+ str(z+1))
+	c.setFont("Times-Roman",14)
+	#Array to be printed
+	W = width/(len(ar[0])+2)
+	H = (height-inch)/(len(ar)+2)
+
+	#Title of the Graph
+	#c.drawString(width/3,-.9*inch,"Title of the Plot")
+	c.drawCentredString(width/2, -.9*inch, "Title of the Plot")
+
+	c.setFont("Times-Roman", 16-.1*len(ar))
+
+	#Title of X and Y axes
+	title=['X Value']
+	for i in xrange(len(ar[0])-1):
+		title.append('Y ' + str(i+1) + " Value")
+	for i in xrange(len(ar[0])):
+		c.drawCentredString((i+1.5)*W,-inch-.7*H,title[i])
+
+	#Size of the Grid
+	xlist=[]
+	for i in xrange(len(ar[0])+1):
+		xlist.append(W*(i+1))
+	ylist=[]
+	for i in xrange(len(ar)+2):
+		ylist.append(-i*H-inch)
+	#Creating the Grid
+	c.grid(xlist, ylist)
+
+	#Printing the Output Array
+	for i in xrange(len(ar)):
+		for j in xrange(len(ar[0])):
+			c.drawCentredString((j+1.5)*W,-H*(1.7+i)-inch,str(ar[i][j]))
+
+	c.showPage()
+
+
+	#Save PDF
+	c.save()
+	z=z+1
+
+img = 'b.jpg'
 ar=np.zeros((100,8))
 ar[2][4]=2985.5258
-W = width/(len(ar[0])+2)
-H = (height-inch)/(len(ar)+2)
-
-#Title of the Graph
-#c.drawString(width/3,-.9*inch,"Title of the Plot")
-c.drawCentredString(width/2, -.9*inch, "Title of the Plot")
-
-c.setFont("Times-Roman", 16-.1*len(ar))
-
-#Title of X and Y axes
-title=['X Value']
-for i in xrange(len(ar[0])-1):
-	title.append('Y ' + str(i+1) + " Value")
-for i in xrange(len(ar[0])):
-	c.drawCentredString((i+1.5)*W,-inch-.7*H,title[i])
-
-#Size of the Grid
-xlist=[]
-for i in xrange(len(ar[0])+1):
-	xlist.append(W*(i+1))
-ylist=[]
-for i in xrange(len(ar)+2):
-	ylist.append(-i*H-inch)
-#Creating the Grid
-c.grid(xlist, ylist)
-
-#Printing the Output Array
-for i in xrange(len(ar)):
-	for j in xrange(len(ar[0])):
-		c.drawCentredString((j+1.5)*W,-H*(1.7+i)-inch,str(ar[i][j]))
-
-c.showPage()
-
-
-#Save PDF
-c.save()
-for i in xrange(len(ar[0])-1):
-	title.append('Y ' + str(i+1) + " Value")
-for i in xrange(len(ar[0])):
-	c.drawCentredString((i+1.5)*W,-inch-.7*H,title[i])
-
-#Size of the Grid
-xlist=[]
-for i in xrange(len(ar[0])+1):
-	xlist.append(W*(i+1))
-ylist=[]
-for i in xrange(len(ar)+2):
-	ylist.append(-i*H-inch)
-#Creating the Grid
-c.grid(xlist, ylist)
-
-#Printing the Output Array
-for i in xrange(len(ar)):
-	for j in xrange(len(ar[0])):
-		c.drawCentredString((j+1.5)*W,-H*(1.7+i)-inch,str(ar[i][j]))
-
-c.showPage()
-
-
-#Save PDF
-c.save()
+z=0
+pdfoutput(img,ar)
+pdfoutput(img,ar)
