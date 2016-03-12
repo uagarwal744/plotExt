6c31e352bd0fadbf87d62f5f125bb1cbc62a1996
Sayan <sgdgp.2014@gmail.com>
2016-03-12 22:20:55 +0530

    Debugging issue with GUI integration

--- graphextract_returnArray.py
+++ graphextract_returnArray.py
@@ -20,6 +20,9 @@ class PlotExtractor:
         density -- parameter for the quality of image. TO BE ENTERED IN STRING FORMAT
         """
         self.pagelist = pagelist
+
+        print self.pagelist
+        
         #self.pdfFile = pdfFile
         self.minLineLength = minLineLength
         self.maxLineGap = maxLineGap
@@ -222,8 +225,8 @@ class PlotExtractor:
 def main():
     """Main function to execute. Put name of image in the first parameter of constructor"""
     pdfName  = raw_input()
-    #G = PlotExtractor(pdfName,200,50,1500,70)
-    #ga = G.graphextract()
+    G = PlotExtractor(pdfName,200,50,1500,70)
+    ga = G.graphextract()
 
 
 if __name__=='__main__':
